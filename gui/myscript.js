578ac370cc3195d44f09156c2e3f2c8cbffa7fc4
Varun Bharill <varunbharill@Varuns-MacBook-Pro.local>
2016-05-23 00:41:15 -0700

    Initial gui.

---
+++ gui/myscript.js
@@ -0,0 +1,32 @@
+document.getElementById("filecontent").addEventListener("keydown", checkInput);
+
+
+
+function checkInput(event){
+    console.log(event.keyCode)
+    console.log(document.getElementById("filecontent").selectionStart);
+}
+
+
+function insertChar() {
+	console.log("insertChar called")
+	var char = 'x'
+	var pos = document.getElementById("pos")
+	//take char and pos as input.
+	var textElem = document.getElementById("filecontent");
+	var content = textElem.value;
+	var currentPos = textElem.selectionStart;
+	var finalPos = -1;
+	if(pos > content.length) {
+		return;
+	}
+	if(pos <= currentPos) {
+		finalPos = currentPos + 1;
+	} else {
+		finalPos = currentPos
+	}
+	
+	textElem.value = (content.substring(0,pos) + char + content.substring(pos))
+	textElem.setSelectionRange(finalPos,finalPos);
+
+}
