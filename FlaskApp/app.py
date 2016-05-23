3bcbdc0b931614560511f620b4d534bbe13e6aa1
Akshay Jain <akshaybetala@gmail.com>
2016-05-23 02:15:40 -0700

    Implementing PPS

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,16 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-	
+
+pps = [[1,'a',True],[2,'b',True],[3,'c',True],[4,'d',True]]
+
 @app.route("/")
 def main():
 	return render_template('index.html')
 
-@app.route('/_test')
+@app.route('/_load',methods=['POST'])
 def add_numbers():
-    a = request.args.get('a', 0, type=int)
-    print a
-    return jsonify(result=a)
-
+    return jsonify(result=pps)
 
 if __name__ == "__main__":
 	app.run()
