5a461bcaaf92ae96ef170546d88879ec682d817f
Varun Bharill <varunbharill@Varuns-MacBook-Pro.local>
2016-05-28 20:29:57 -0700

    changes in myscript.

--- FlaskApp/static/myscript.js
+++ FlaskApp/static/myscript.js
@@ -1,3 +1,5 @@
+pps = []
+
 document.getElementById("filecontent").addEventListener("keydown", checkInput);
 document.addEventListener("DOMContentLoaded", function() {
  
@@ -6,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function() {
             type: 'POST',
             success: function(response) {
                 console.log(response);
+
+                initPPSAndEditor(response.result);
             },
             error: function(error) {
                 console.log(error);
@@ -21,14 +25,58 @@ function checkInput(event){
     //console.log(document.getElementById("filecontent").selectionStart);
 }
 
+function initPPSAndEditor(iniDoc) {
+	document.getElementById("filecontent").value = ""	
+	editorPos = 0;
+	for(i = 0; i < iniDoc.length ; i++ ) {
+		entry = iniDoc[i]
+		insertOrEditEntryInPPS(entry);
+		if(entry[2] == true) {
+			insertChar(entry[1],editorPos);
+			editorPos++;
+		}
+	}
+	console.log(pps)
+}
 
-function insertChar() {
+function insertOrEditEntryInPPS(entry) {
+	editorPos = 0;
+	if(pps.length == 0) {
+		pps.push(entry);
+		return editorPos;
+	}
+	tempPPIListPosition = 0;
+
+	for(e in pps) {
+		if(e[2] == true) {
+			editorPos++;
+		}
+		if(e[0] == entry[0]) {
+			e[1] = entry[1];
+			e[2] = entry[2];
+			return -1;
+		}
+		else if(e[0] < entry[0]) {
+			tempPPIListPosition++;
+		} 
+		else {
+			break;
+		}
+	}
+	pps.splice(tempPPIListPosition,0,entry);
+	return editorPos;
+
+}
+
+function insertChar(char, pos) {
 	console.log("insertChar called")
-	var char = 'x'
-	var pos = document.getElementById("pos").value
+
+	// var char = 'x'
+	// var pos = document.getElementById("pos").value
 	//take char and pos as input.
-	console.log(pos)
+	//console.log(pos)
 	var textElem = document.getElementById("filecontent");
+	//console.log("length before inserting - " + textElem.value.length)
 	var content = textElem.value;
 	var currentPos = textElem.selectionStart;
 	var finalPos = -1;
@@ -42,6 +90,28 @@ function insertChar() {
 	}
 	
 	textElem.value = (content.substring(0,pos) + char + content.substring(pos))
+	textElem.setSelectionRange(finalPos,finalPos);
+	//console.log("length before inserting - " + textElem.value.length)
+}
+
+function deleteChar(pos) {
+	console.log("deleteChar called")
+	//var pos = document.getElementById("pos").value
+	console.log(pos)
+	var textElem = document.getElementById("filecontent");
+	var content = textElem.value;
+	var currentPos = textElem.selectionStart;
+	var finalPos = -1;
+	if(pos > content.length) {
+		return;
+	}
+	if(pos <= currentPos) {
+		finalPos = currentPos - 1;
+	} else {
+		finalPos = currentPos
+	}
+	
+	textElem.value = (content.substring(0,pos) + content.substring(pos+1))
 	textElem.setSelectionRange(finalPos,finalPos);
 
 }
