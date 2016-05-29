6359a269172c050fa332750d0e6df0eb11726a5e
Varun Bharill <varunbharill@Varuns-MacBook-Pro.local>
2016-05-29 01:26:08 -0700

    ajax call.

--- FlaskApp/static/myscript.js
+++ FlaskApp/static/myscript.js
@@ -1,5 +1,5 @@
 pps = []
-
+my_client_id = 0
 document.getElementById("filecontent").addEventListener("keydown", checkInput);
 
 
@@ -12,7 +12,17 @@ $(document).ready(function(){
     });
 
     socket.on('server-operation',function(operation){
+    	if(my_client_id != operation.client_id) {
+    		return
+    	}
     	console.log(operation);
+    	entry = operation.value
+    	pos = insertOrEditEntryInPPS(entry)
+    	if(operation.type = "insert") {
+    		insertChar(entry[1],pos)
+    	} else {
+			deleteChar(pos)    		 
+    	}
 
     });
 
@@ -24,7 +34,62 @@ $(document).ready(function(){
 
 function checkInput(event){
     //console.log(event.keyCode)
-    //console.log(document.getElementById("filecontent").selectionStart);
+    //console.log(document.getElementById("filecontent").selectionStart + 1);
+    pos = document.getElementById("filecontent").selectionStart;
+    key = event.keyCode
+    if(event.keyCode == '8') {
+    	if(pos == 0) {
+    		return;
+    	}  
+    	dict = {}
+    	dict['client_id'] = my_client_id 	
+    	dict['ppi'] = getDeletePPIPos(pos)
+    	console.log("deleye called at pos" + pos);
+    	$.ajax({url: "demo_ajax_script.js", dataType: "script"});
+    } else {
+    	if(key > 90 || key < 65) {
+    		return;
+    	}
+    	console.log("char inserted at pos - " + (pos +1) + " " + key )
+	    dict = {}
+	    ppi_interval = getPPIInterval(pos);
+	    dict['type'] = "insert";
+	    dict['start_ppi'] = ppi_interval[0];
+	    dict['end_ppi'] = ppi_interval[1]; 
+	    dict['value'] = event.keyCode;
+	    dict['client_id'] = my_client_id
+	    response = $.ajax({url: "/url", data : dict});
+    }
+    
+}
+
+function getDeletePPIPos(pos) {
+	temp = 0
+	for (i = 0; i < pps.length; i++) {
+		if(pps[i][2] == true) {
+			if(pos == temp ) {
+				return ppi[i][0]
+			} else {
+				temp++;
+			}
+
+		}
+	}
+}
+
+function getPPIInterval(pos) {
+	var ppi_interval = [-2,-2];
+	for (i = 0; i < pps.length ; i++) {
+		entry = pps[i];
+		if(entry[0] < pos) {
+			ppi_interval[0]  = entry[0];
+		}
+		else if(entry[0] > pos) {
+			ppi_interval[1] = entry[0]
+		}
+	}
+
+	return ppi_interval
 }
 
 function initPPSAndEditor(iniDoc) {
@@ -32,8 +97,10 @@ function initPPSAndEditor(iniDoc) {
 	editorPos = 0;
 	for(i = 0; i < iniDoc.length ; i++ ) {
 		entry = iniDoc[i]
+		console.log(entry);
 		insertOrEditEntryInPPS(entry);
 		if(entry[2] == true) {
+
 			insertChar(entry[1],editorPos);
 			editorPos++;
 		}
